116046a69e1dfca68a98bfd3c391ffd4a5650adc
rotex <dantejake48@gmail.com>
2023-05-03 18:32:21 +0100

    added execution permission

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -31,20 +31,23 @@ checker issues
     if n < 1:
         return 0
 
-    # Initialize a list to store the minimum number
-    # of operations required for each state
-    # We start with the initial state (1 H character)
-    # and compute subsequent states until n
+    Initialize a list to store the minimum number
+    of operations required for each state
+    We start with the initial state (1 H character)
+    and compute subsequent states until n
+
     dp = [0] + [float('inf')] * (n - 1)
 
     for i in range(2, n + 1):
-        # If i is a prime number, we cannot reach this
-        # state by copying and pasting
+        If i is a prime number, we cannot reach this
+        state by copying and pasting
+
         if is_prime(i):
             continue
 
-        # Find the minimum number of operations
-        # required to reach state i
+        Find the minimum number of operations
+        required to reach state i
+
         for j in range(i - 1, 0, -1):
             if i % j == 0:
                 dp[i - 1] = min(dp[i - 1], dp[j - 1] + i // j)
