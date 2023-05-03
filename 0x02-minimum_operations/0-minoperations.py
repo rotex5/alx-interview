64de819c8e7d681336e14026f056978f5ee7cc3e
rotex <dantejake48@gmail.com>
2023-05-03 18:29:41 +0100

    minimum operations fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,6 +7,27 @@ Learning Dynamic programming
 
 
 def minOperations(n: int) -> int:
+    """
+    calculates the fewest number of
+    operations needed to result in exactly n H
+    characters in this file.
+    """
+    if n <= 1:
+        return 0
+
+    dp: int = 0
+    count: int = 2
+    while (count <= n):
+        if not (n % count):
+            n = int(n / count)
+            dp += count
+            count = 1
+        count += 1
+    return dp
+
+
+"""
+checker issues
     if n < 1:
         return 0
 
@@ -38,3 +59,4 @@ def is_prime(n: int) -> bool:
         if n % i == 0:
             return False
     return True
+"""
