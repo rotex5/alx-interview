d1f4ebce276f51d9e24e49063f9577a22814006f
rotex <dantejake48@gmail.com>
2023-05-12 02:13:53 +0100

    test

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,8 +2,6 @@
 """
 Script that reads stdin line by line and computes metrics:
 """
-
-
 import sys
 from collections import defaultdict
 
@@ -12,12 +10,10 @@ STATUS_CODE_COUNTS = defaultdict(int)
 LINE_COUNT = 0
 
 for line in sys.stdin:
-    # print(line)
     try:
         _, _, _, _, _, _, _, status_code_str, file_size_str = line.split()
         status_code = int(status_code_str)
         file_size = int(file_size_str)
-        # print(status_code, file_size)
     except ValueError:
         continue
 
@@ -31,21 +27,3 @@ for line in sys.stdin:
             count = STATUS_CODE_COUNTS[status_code]
             if count > 0:
                 print(f"{status_code}: {count}")
-
-    try:
-        # Check if user pressed CTRL + C
-        if sys.stdin.isatty() and sys.stdin.read(1) == '\x03':
-            print(f"Total file size: {TOTAL_SIZE}")
-            for status_code in sorted(STATUS_CODE_COUNTS.keys()):
-                count = STATUS_CODE_COUNTS[status_code]
-                if count > 0:
-                    print(f"{status_code}: {count}")
-            sys.exit(0)
-    except KeyboardInterrupt:
-        # User pressed CTRL + C
-        print(f"Total file size: {TOTAL_SIZE}")
-        for status_code in sorted(STATUS_CODE_COUNTS.keys()):
-            count = STATUS_CODE_COUNTS[status_code]
-            if count > 0:
-                print(f"{status_code}: {count}")
-        sys.exit(0)
