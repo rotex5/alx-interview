7a9a6ee48e8643dab2ca4e462552208a9442ae89
rotex <dantejake48@gmail.com>
2023-05-12 02:21:25 +0100

    test

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,25 +5,46 @@ Script that reads stdin line by line and computes metrics:
 import sys
 from collections import defaultdict
 
-TOTAL_SIZE = 0
-STATUS_CODE_COUNTS = defaultdict(int)
-LINE_COUNT = 0
+
+total_size = 0
+status_code_counts = defaultdict(int)
+line_count = 0
 
 for line in sys.stdin:
+    # print(line)
     try:
         _, _, _, _, _, _, _, status_code_str, file_size_str = line.split()
         status_code = int(status_code_str)
         file_size = int(file_size_str)
+        # print(status_code, file_size)
     except ValueError:
         continue
 
-    TOTAL_SIZE += file_size
-    STATUS_CODE_COUNTS[status_code] += 1
-    LINE_COUNT += 1
+    total_size += file_size
+    status_code_counts[status_code] += 1
+    line_count += 1
 
-    if LINE_COUNT % 10 == 0:
-        print(f"Total file size: {TOTAL_SIZE}")
-        for status_code in sorted(STATUS_CODE_COUNTS.keys()):
-            count = STATUS_CODE_COUNTS[status_code]
+    if line_count % 10 == 0:
+        print(f"Total file size: {total_size}")
+        for status_code in sorted(status_code_counts.keys()):
+            count = status_code_counts[status_code]
             if count > 0:
                 print(f"{status_code}: {count}")
+
+    try:
+        # Check if user pressed CTRL + C
+        if sys.stdin.isatty() and sys.stdin.read(1) == '\x03':
+            print(f"Total file size: {total_size}")
+            for status_code in sorted(status_code_counts.keys()):
+                count = status_code_counts[status_code]
+                if count > 0:
+                    print(f"{status_code}: {count}")
+            sys.exit(0)
+    except KeyboardInterrupt:
+        # User pressed CTRL + C
+        print(f"Total file size: {total_size}")
+        for status_code in sorted(status_code_counts.keys()):
+            count = status_code_counts[status_code]
+            if count > 0:
+                print(f"{status_code}: {count}")
+        sys.exit(0)
