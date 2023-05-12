ea1542164bd1f16c3fb923ef65edec303ce09b37
rotex <dantejake48@gmail.com>
2023-05-12 02:09:30 +0100

    reducing doc string

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,49 +1,6 @@
 #!/usr/bin/python3
 """
-
--Write a script that reads stdin line by line and computes metrics:
-
--Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
-    <status code> <file size> (if the format is not this one, the
-                           line must be skipped)
-
--After every 10 lines and/or a keyboard interruption (CTRL + C),
-    print these statistics from the beginning:
-*Total file size: File size: <total size>
-*where <total size> is the sum of all previous <file size>
-    (see input format above)
-
-*Number of lines by status code:
-+possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-+if a status code doesn’t appear or is not an integer, don’t print
-    anything for this status code
-+format: <status code>: <number>
-+status codes should be printed in ascending order
-
-Log parsing
-Reads stin line by line and computes metrics
-(i.e <status code> and <file size>)
-
-'104.245.160.235 - [2023-05-12 01:45:51.041606]
-"GET /projects/260 HTTP/1.1" 200 251'
-
-output:
-File size: 5213
-200: 2
-401: 1
-403: 2
-404: 1
-405: 1
-500: 3
-File size: 11320
-200: 3
-301: 2
-400: 1
-401: 2
-403: 3
-404: 4
-405: 2
-500: 3
+Script that reads stdin line by line and computes metrics:
 """
 import sys
 from collections import defaultdict
