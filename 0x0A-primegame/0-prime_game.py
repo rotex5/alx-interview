ece6232b36797e7d368f8d52bb87f7e1ca9e4b61
rotex <dantejake48@gmail.com>
2023-06-28 07:39:30 +0100

    test run

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -16,46 +16,6 @@ def isWinner(x, nums):
       str: Name of the player who won the most rounds.
            If the winner cannot be determined, returns None.
     """
-    def is_prime(num):
-        """
-        Checks if a given number is prime.
-
-        Args:
-            num (int): The number to check.
-
-        Returns:
-            bool: True if the number is prime, False otherwise.
-        """
-        if num < 2:
-            return False
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-
-    def c_primes(n):
-        """
-        Counts the number of prime numbers up to and including n.
-
-        Args:
-        n (int): The upper limit.
-
-        Returns:
-        int: The count of prime numbers.
-        """
-        if n < 2:
-            return 0
-
-        primes = [True] * (n + 1)
-        primes[0] = primes[1] = False
-
-        for i in range(2, int(n ** 0.5) + 1):
-            if primes[i]:
-                for j in range(i * i, n + 1, i):
-                    primes[j] = False
-
-        return sum(primes)
-
     if not nums or x < 1:
         return None
 
@@ -69,3 +29,47 @@ def isWinner(x, nums):
     if wins % 2 == 0:
         return "Ben"
     return "Maria"
+
+
+def c_primes(n):
+    """
+    Counts the number of prime numbers up to and including n.
+
+    Args:
+      n (int): The upper limit.
+
+    Returns:
+      int: The count of prime numbers.
+    """
+    if n < 2:
+        return 0
+
+    primes = [True] * (n + 1)
+    primes[0] = primes[1] = False
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, n + 1, i):
+                primes[j] = False
+
+    return sum(primes)
+
+
+def prime_num(num):
+    """
+    Checks if a number is prime.
+
+    Args:
+      num (int): The number to check.
+
+    Returns:
+      bool: True if the number is prime, False otherwise.
+    """
+    if num < 2:
+        return False
+
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+
+    return True
